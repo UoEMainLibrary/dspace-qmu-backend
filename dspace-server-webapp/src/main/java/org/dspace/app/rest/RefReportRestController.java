9809c830037ad8b9cbd8707286b0120a3789288c
David Speed <david.speed@ed.ac.uk>
2025-06-05 10:23:29 +0100

    #LUC065-103 Attempt at adding a new solr field and updated ref terms query to use it

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/RefReportRestController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/RefReportRestController.java
@@ -98,12 +98,12 @@ public class RefReportRestController implements InitializingBean {
                 items = this.getAuthorItems(context, author);
             } if (StringUtils.isNotEmpty(endDateString)) {
                 log.info("Get the items for endDate metadata {}", endDateString);
-                String query = "refterms.dateAccepted:[* TO " + endDateString + "T00:00:00Z" + "]";
+                String query = "refterms.dateAccepted_dt:[* TO " + endDateString + "T00:00:00Z" + "]";
                 log.info("Query {}", query);
                 items = this.getDateItems(context, query);
             } if (StringUtils.isNotEmpty(startDateString)) {
                 log.info("Get the items for startDate metadata {}", startDateString);
-                String query = "refterms.dateAccepted:[" + startDateString + "T00:00:00Z" + " TO *]";
+                String query = "refterms.dateAccepted_dt:[" + startDateString + "T00:00:00Z" + " TO *]";
                 log.info("Query {}", query);
                 items = this.getDateItems(context, query);
             }
